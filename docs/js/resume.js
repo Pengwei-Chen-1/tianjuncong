0958324f2eb54069ffaf219d3a3f7ccf2ce844ef
tianjuncong <14567357892qq.com>
2016-10-15 17:35:36 +0800

    commit new

---
+++ docs/js/resume.js
@@ -0,0 +1,18 @@
+//resume
+
+// ±³¾°Í¼Æ¬ÂÖ²¥
+
+$(window).load(function(){
+    $('.preloader').delay(1000).fadeOut("slow"); // set duration in brackets    
+});
+
+// HOME BACKGROUND SLIDESHOW
+$(function(){
+    jQuery(document).ready(function() {
+		$('body').backstretch([
+	 		 "images/bg-slide-1.jpg",
+	 		 "images/bg-slide-2.jpg",
+			 "images/bg-slide-3.jpg"
+	 			], 	{duration: 3200, fade: 1300});
+		});
+})
